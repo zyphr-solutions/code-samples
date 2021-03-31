03ead75888e24db9d7c75eb9b1f5b6a920bb0047
Aanika Rahman <aanikarahman@gmail.com>
2021-03-31 14:26:47 -0400

    define app outside of function

--- app.py
+++ app.py
@@ -8,27 +8,15 @@ from flask_bcrypt import Bcrypt
 from utils.constants import APP_CSS
 
 
-bcrypt = Bcrypt()
+app = dash.Dash(
+    __name__, external_stylesheets=APP_CSS, suppress_callback_exceptions=True
+)
+server = app.server
+server.config.from_object("config.Config")
 
+bcrypt = Bcrypt(server)
 
-def init_app():
-    """
-    Configure Dash app and Flask server.
-    """
-    app = dash.Dash(
-        __name__, external_stylesheets=APP_CSS, suppress_callback_exceptions=True
-    )
-    server = app.server
-    server.config.from_object("config.Config")
+with server.app_context():
+    from auth import auth_blueprint
 
-    bcrypt.init_app(server)
-
-    with server.app_context():
-        from auth import auth_blueprint
-
-        server.register_blueprint(auth_blueprint)
-
-    return app
-
-
-app = init_app()
+    server.register_blueprint(auth_blueprint)
